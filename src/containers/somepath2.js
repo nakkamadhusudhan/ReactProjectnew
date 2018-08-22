200cd4193eb6e02692f11ec05941bc0d7f4eb6a2
nakkamadhusudhan <42340507+nakkamadhusudhan@users.noreply.github.com>
2018-08-22 20:23:18 +0530

    Update somepath2.js

--- src/containers/somepath2.js
+++ src/containers/somepath2.js
@@ -3,13 +3,16 @@ import {connect} from 'react-redux';
 import Button from './../components/buttons/Button'
 class somepath2 extends Component{
 
+    enrollMerchant = () => {
+        this.props.history.push("/");
+    }
     
     render(){
         return(
-            <div style={{textAlign:"center",color:"red",fontFamily:"sans-serif",fontSize:"30px", border: "2px solid #1d1da0fa",lineHeight : "1.6",width:"400px",  margin: "144px auto",padding:"20px"}}>
+            <div style={NewStyle}>
                  Sorry,your enrollment could <br/>not be complete online,please contact clint care.
                   <br/>
-                  <Button />
+                  <Button buttonClass="btn btn-primary" onClick={this.enrollMerchant} buttonName="Contact Clint Care"/>
             </div>
         );
     }
@@ -22,5 +25,17 @@ const mapStateToProps = State =>({
 const mapStateToDispatch = Dispatch =>({
 
 });
+
+const NewStyle={
+    textAlign:"center",
+    color:"red",
+    fontFamily:"sans-serif",
+    fontSize:"30px", 
+    border:"3px solid #071848",
+    lineHeight : "1.6",
+    width:"400px", 
+     margin: "144px auto",
+     padding:"20px",
+}
 
 export default connect (mapStateToProps,mapStateToDispatch)(somepath2);
